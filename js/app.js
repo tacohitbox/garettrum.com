d7ff20cac78beb684098bb81f14b1de4e187484e
aria <mrsariablue@pm.me>
2022-01-30 19:28:53 -0500

    add sites

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var imgs = [
+  toImg('1.jpg'),
+  toVid('2.mov'),
+  toImg('3.jpg'),
+  toImg('4.jpg'),
+  toImg('5.jpg'),
+  toImg('6.jpg'),
+  toImg('7.jpg'),
+  toImg('8.jpg'),
+  toImg('9.jpg'),
+  toImg('10.png'),
+  toImg('11.png'),
+  toImg('12.png')
+]
+
+document.querySelector(".gart").innerHTML = imgs[Math.floor(Math.random()*imgs.length)];
+
+function toImg(src) {
+  return `<img alt="Image of Garett." src="/assets/images/${src}">`;
+}
+
+function toVid(src) {
+  return `<video autoplay controls loop src="/assets/images/${src}"></video>`;
+}
